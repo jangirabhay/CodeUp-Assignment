dc4e2acc5ceca588957c4e7655821f5707ff45d6
Abhay0506 <168776158+jangirabhay@users.noreply.github.com>
2025-11-06 09:53:16 +0530

    1 week assignment

---
+++ src/UserError.java
@@ -0,0 +1,6 @@
+public class UserError extends MyArray {
+    public final String errorInput = "Given data is invalid ";
+    public String inValid(){
+        return "Given data is invalid";
+    }
+}
